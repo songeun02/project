640f5cf00a845721e33b4fb2d6565de36afeee8b
songeun02 <se021216@yu.ac.kr>
2025-03-11 00:05:50 +0900

    Update analyze_2020.R

--- 2023년 지역 치안 안전 데이터 분석 경진대회/analyze_2020.R
+++ 2023년 지역 치안 안전 데이터 분석 경진대회/analyze_2020.R
@@ -15,9 +15,11 @@ data_happen
 View(data_happen)
 dim(data_happen)
 
-data1_daejeon <- subset(data_happen,NPA_CL=='13'& substr(HPPN_OLD_ADDR,1,2)=='대전') # 경찰청구분 = 대전 
+# 경찰청구분 = 대전 
+data1_daejeon <- subset(data_happen,NPA_CL=='13'& substr(HPPN_OLD_ADDR,1,2)=='대전') 
 View(data1_daejeon)
 
+# 각 지역별 교통사고량 확인
 row_num1 <- nrow(data2020)
 daejeon_count <- 0
 sejong_count <- 0
@@ -31,12 +33,14 @@ for(i in 1:row_num1){
     chungnam_count <- chungnam_count+1
   }
 }
+
 happen_count <- c(daejeon_count,sejong_count,chungnam_count)
 happen_count
-area_happen_2020<- data.frame(data2020,area_2020) # 대전 
-View(area_happen_2020) 
 
-data1_daejeon_excpet_HPPN_0 <- subset(data1_daejeon,HPPN_Y != '0') # 대전 
+# 충남은 지역이 너무 커서 교통 사고량이 많다고 판단.
+# 대전은 충남보다 훨씬 작지만 세종과는 비슷하다고 판단.
+# 세종과 비슷한 지역 넓이지만 교통 사고량이 많이 차이 나서 대전 분석 진행 
+data1_daejeon_excpet_HPPN_0 <- subset(data1_daejeon,HPPN_Y != '0')  
 
 data1_daejeon_map_address <-subset(data1_daejeon_excpet_HPPN_0,select =c(RECV_CPLT_DT,HPPN_X,HPPN_Y) ) 
 data1_daejeon_map_address
@@ -45,15 +49,24 @@ View(data1_daejeon_map_address)
 row_num <- nrow(data1_daejeon_map_address)
 my_daejeon_2020 <- c()
 for(i in 1:row_num){
+  # 년도와 월 추출 
   my_daejeon_2020[i] <- substr(data1_daejeon_map_address$RECV_CPLT_DT[i],1,6)
-} # 대전 
+}  
 
-data1_daejeon_map_address_0<- data.frame(data1_daejeon_map_address,my_daejeon_2020) # 대전 
+data1_daejeon_map_address_0<- data.frame(data1_daejeon_map_address,my_daejeon_2020) 
 View(data1_daejeon_map_address_0) 
 
 count_happen_daejeon_2020_month <- table(data1_daejeon_map_address_0$my)
-barplot(count_happen_daejeon_2020_month)
+count_happen_daejeon_2020_month
+month_plot = barplot(count_happen_daejeon_2020_month, names=c("1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월"),ylim=c(0,3500), main="2020년 월별 교통사고량")
+# 그래프에 값 출력
+text(x=month_plot, y=count_happen_daejeon_2020_month,labels=count_happen_daejeon_2020_month,pos=3,col="black")
 
+# 달마다 발생하는 교통사고 평균 수 
+mean(count_happen_daejeon_2020_month)
+count_happen_daejeon_2020_month - mean(count_happen_daejeon_2020_month)
+
+# 구글맵 시각화 
 install.packages('ggmap')
 ggmap_key <- 'AIzaSyC0KeTEeGxabXPdPUEcZZQHVMtHt7RSQiE'
 library(ggmap)
